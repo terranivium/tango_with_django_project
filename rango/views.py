fd37e57b50e2cf544764c7a484c1a6380de50237
terranivium <wesleyscottuk@gmail.com>
2020-01-19 16:32:05 +0000

    Completed up to Task 4, todo: run unit testing

--- rango/views.py
+++ rango/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Rango says hey there partner!")
+	return HttpResponse("Rango says hey there partner!" + "<a href='/rango/about/'>About</a>")
+
+def about(request):
+	return HttpResponse("Here be the about page brother!" + "<a href='/rango/'>Index</a>")
