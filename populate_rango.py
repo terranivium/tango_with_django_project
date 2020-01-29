40b9f1b4c7f2ad74399a849861351c9acb27e98c
terranivium <wesleyscottuk@gmail.com>
2020-01-29 15:59:42 +0000

    Passed tests_chapter6

--- populate_rango.py
+++ populate_rango.py
@@ -16,29 +16,43 @@ def populate():
 
 	python_pages = [
 		{'title': 'Official Python Tutorial',
-		 'url':'http://docs.python.org/3/tutorial/'},
+		 'url':'http://docs.python.org/3/tutorial/',
+		 'views':3},
 		{'title':'How to Think like a Computer Scientist',
-		 'url':'http://www.greenteapress.com/thinkpython/'},
+		 'url':'http://www.greenteapress.com/thinkpython/',
+		 'views':3},
 		{'title':'Learn Python in 10 Minutes',
-		 'url':'http://www.korokithakis.net/tutorials/python/'} ]
+		 'url':'http://www.korokithakis.net/tutorials/python/',
+		 'views':3} ]
 
 	django_pages = [
 		{'title':'Official Django Tutorial',
-		 'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+		 'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
+		 'views':3},
 		{'title':'Django Rocks',
-		 'url':'http://www.djangorocks.com/'},
+		 'url':'http://www.djangorocks.com/',
+		 'views':3},
 		{'title':'How to Tango with Django',
-		 'url':'http://www.tangowithdjango.com/'} ]
+		 'url':'http://www.tangowithdjango.com/',
+		 'views':3} ]
 
 	other_pages = [
 		{'title':'Bottle',
-		 'url':'http://bottlepy.org/docs/dev/'},
+		 'url':'http://bottlepy.org/docs/dev/',
+		 'views':3},
 		{'title':'Flask',
-		 'url':'http://flask.pocoo.org'} ]
+		 'url':'http://flask.pocoo.org',
+		 'views':3} ]
 
-	cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
-			'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
-			'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16 } }
+	cats = {'Python': {'pages': python_pages, 
+			'views': 128, 
+			'likes': 64},
+			'Django': {'pages': django_pages, 
+			'views': 64, 
+			'likes': 32},
+			'Other Frameworks': {'pages': other_pages, 
+			'views': 32, 
+			'likes': 16 } }
 
 	# If you want to add more categories or pages,
 	# add them to the dictionaries above.
