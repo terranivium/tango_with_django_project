6d7d2279d08b178fb7b903053637f5a4ed3fcecb
terranivium <wesleyscottuk@gmail.com>
2020-01-29 18:53:52 +0000

    Fixed tests_chapter6 failures, repo now passes tests_chapter3-7

--- populate_rango.py
+++ populate_rango.py
@@ -17,13 +17,13 @@ def populate():
 	python_pages = [
 		{'title': 'Official Python Tutorial',
 		 'url':'http://docs.python.org/3/tutorial/',
-		 'views':3},
+		 'views':12},
 		{'title':'How to Think like a Computer Scientist',
 		 'url':'http://www.greenteapress.com/thinkpython/',
-		 'views':3},
+		 'views':12},
 		{'title':'Learn Python in 10 Minutes',
 		 'url':'http://www.korokithakis.net/tutorials/python/',
-		 'views':3} ]
+		 'views':12} ]
 
 	django_pages = [
 		{'title':'Official Django Tutorial',
@@ -62,7 +62,7 @@ def populate():
 	for cat, cat_data in cats.items():
 		c = add_cat(cat,cat_data['views'],cat_data['likes'])
 		for p in cat_data['pages']:
-			add_page(c, p['title'], p['url'])
+			add_page(c, p['title'], p['url'], p['views'])
 
 	# Print out the categories we have added.
 	for c in Category.objects.all():
--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from rango.models import Category
-from rango.models import Page
+from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm
+from django.urls import reverse
 
 # Create your views here.
 
@@ -14,10 +15,25 @@ def index(request):
 	context_dict = {}
 	context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
 	context_dict['categories'] = category_list
+	context_dict['pages'] = page_list
 
 	# Return a rendered response to send to the client.
 	# We make use of the shortcut function to make our lives easier. # Note that the first parameter is the template we wish to use.
 	return render(request, 'rango/index.html', context=context_dict)
+
+def add_category(request):
+	form = CategoryForm()
+
+	if request.method == 'POST':
+		form = CategoryForm(request.POST)
+
+		if form.is_valid():
+			form.save(commit=True)
+			return redirect('/rango/')
+		else:
+			print(form.errors)
+
+	return render(request, 'rango/add_category.html', {'form':form})
 
 def show_category(request, category_name_slug):
 	context_dict = {}
@@ -27,13 +43,44 @@ def show_category(request, category_name_slug):
 
 		pages = Page.objects.filter(category=category)
 
-		context_dict['pages'] = pages
 		context_dict['category'] = category
+		context_dict['pages'] = pages
 	except Category.DoesNotExist:
-		context_dict['pages'] = None
 		context_dict['category'] = None
+		context_dict['pages'] = None
 
 	return render(request, 'rango/category.html', context=context_dict)
+
+def add_page(request, category_name_slug):
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+	except Category.DoesNotExist:
+		category = None
+
+	if category is None:
+		return redirect('/rango/')
+
+	form = PageForm()
+
+	if request.method == 'POST':
+		form = PageForm(request.POST)
+
+		if form.is_valid():
+			if category:
+				page = form.save(commit=False)
+				page.category = category
+				page.views = 0
+				page.save()
+
+				return redirect(reverse('rango:show_category',
+										kwargs={'category_name_slug':
+											category_name_slug}))
+		else:
+			print(form.errors)
+
+	context_dict = {'form':form,'category':category}
+	return render(request, 'rango/add_page.html', context=context_dict)
+
 
 def about(request):
 	context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
