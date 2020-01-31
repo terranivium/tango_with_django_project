cd0c340c6f1f646c79f4eb277744fd070545aefa
terranivium <wesleyscottuk@gmail.com>
2020-01-31 17:10:10 +0000

    tests_chapter10 passed, todo: passes 3-6, 9, 10 - potential to pass all tests? exit() exit

--- rango/views.py
+++ rango/views.py
@@ -5,12 +5,37 @@ from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 # Create your views here.
 
 @login_required
 def restricted(request):
 	return render(request, 'rango/restricted.html', {})
+
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None): 
+	val = request.session.get(cookie)
+	if not val:
+		val = default_val 
+	return val
+
+# Updated the function definition
+def visitor_cookie_handler(request):
+	visits = int(get_server_side_cookie(request, 'visits', '1')) 
+	last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+	last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+	# If it's been more than a day since the last visit...
+	if (datetime.now() - last_visit_time).days > 0:
+		visits = visits + 1
+		# Update the last visit cookie now that we have updated the count 
+		request.session['last_visit'] = str(datetime.now())
+	else:
+		# Set the last visit cookie 
+		request.session['last_visit'] = last_visit_cookie
+
+	# Update/set the visits cookie
+	request.session['visits'] = visits
 
 def index(request):
 	# Construct a dictionary to pass to the template engine as its context.
@@ -23,9 +48,12 @@ def index(request):
 	context_dict['categories'] = category_list
 	context_dict['pages'] = page_list
 
+	visitor_cookie_handler(request)
+
 	# Return a rendered response to send to the client.
 	# We make use of the shortcut function to make our lives easier. # Note that the first parameter is the template we wish to use.
-	return render(request, 'rango/index.html', context=context_dict)
+	response = render(request, 'rango/index.html', context=context_dict)
+	return response
 
 @login_required
 def add_category(request):
@@ -90,7 +118,11 @@ def add_page(request, category_name_slug):
 	return render(request, 'rango/add_page.html', context=context_dict)
 
 def about(request):
-	context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+	context_dict = {}
+	context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+
+	visitor_cookie_handler(request)
+	context_dict['visits'] = request.session['visits']
 	return render(request, 'rango/about.html', context=context_dict)
 
 def register(request):
